889724467f0804b30253c1398178fac15133d6f0
mlenic23 <martalenic2003@gmail.com>
2025-04-05 22:45:39 +0200

    Login, registration and homepage

--- app/views.py
+++ app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successful registration!')
+            return redirect('login')
+        else:
+            messages.error(request, 'Please fill out all registration fields!')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form':form})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Wrong username or password.')
+    return render(request, 'login.html')
+
+def logout(request):
+    logout(request)
+    return redirect('login')
