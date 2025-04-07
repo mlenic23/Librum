4ad60d5df6db902a8c7af965bd6808d64dc23fc3
mlenic23 <martalenic2003@gmail.com>
2025-04-08 01:16:52 +0200

    Completed login and registration page

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserCreationForm
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django import forms
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
-def register(request):
+def user_register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Successful registration!')
@@ -18,10 +20,10 @@ def register(request):
         else:
             messages.error(request, 'Please fill out all registration fields!')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'register.html', {'form':form})
 
-def login(request):
+def user_login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -33,6 +35,12 @@ def login(request):
             messages.error(request, 'Wrong username or password.')
     return render(request, 'login.html')
 
-def logout(request):
+def user_logout(request):
     logout(request)
     return redirect('login')
+
+def clean_email(self):
+    email = self.cleaned_data.get('email')
+    if User.objects.filter(email=email).exists():
+        raise forms.ValidationError("Email is already in use.")
+    return email
