15a1155a7c17a9b98d91d9b563ee692b2a24bbbf
mlenic23 <martalenic2003@gmail.com>
2025-08-24 17:07:40 +0200

    Changes

--- app/static/js/slider.js
+++ app/static/js/slider.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const yearSlider = document.getElementById('year-slider');
-
-    // Dohvaćamo vrijednosti iz HTML input polja (koje postavlja Django)
     const minYear = parseInt(document.getElementById('min-year-input').value);
     const maxYear = parseInt(document.getElementById('max-year-input').value);
 
-    // Kreiramo slider s dinamičkim rasponom
     noUiSlider.create(yearSlider, {
         start: [minYear, maxYear],
         connect: true,
@@ -14,20 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
             min: minYear,
             max: maxYear
         },
-        tooltips: [true, true],
+        tooltips: [true, true], 
         format: {
             to: value => Math.round(value),
             from: value => Number(value)
         }
     });
 
-    // Elementi za prikaz i unos godina
     const minYearInput = document.getElementById('min-year-input');
     const maxYearInput = document.getElementById('max-year-input');
     const minYearDisplay = document.getElementById('year-min');
     const maxYearDisplay = document.getElementById('year-max');
 
-    // Povezivanje slidera s input poljima i prikazom
     yearSlider.noUiSlider.on('update', function (values) {
         minYearInput.value = values[0];
         maxYearInput.value = values[1];
@@ -35,7 +30,3 @@ document.addEventListener("DOMContentLoaded", function () {
         maxYearDisplay.innerText = values[1];
     });
 });
-
-
-
-
